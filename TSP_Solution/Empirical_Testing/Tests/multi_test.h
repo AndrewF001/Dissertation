0c3b23e95ae9a80fef8a1edcd4f3ce1bbb715833
AndrewF001 <andrewfoot@hotmail.com>
2025-01-27 12:27:42 +0000

    very basic multi-threading

--- TSP_Solution/Empirical_Testing/Tests/multi_test.h
+++ TSP_Solution/Empirical_Testing/Tests/multi_test.h
@@ -8,26 +8,34 @@ public:
 	~MultiTest() = default;
 
 	void Test() override {
+		std::vector<std::thread> threads;
+
 		if (m_max_size > 0)
-			_test<SIZES[0]>();
+			threads.emplace_back([this]() { _test<SIZES[0]>(); });
 		if (m_max_size > 1)
-			_test<SIZES[1]>();
+			threads.emplace_back([this]() { _test<SIZES[1]>(); });
 		if (m_max_size > 2)
-			_test<SIZES[2]>();
+			threads.emplace_back([this]() { _test<SIZES[2]>(); });
 		if (m_max_size > 3)
-			_test<SIZES[3]>();
+			threads.emplace_back([this]() { _test<SIZES[3]>(); });
 		if (m_max_size > 4)
-			_test<SIZES[4]>();
+			threads.emplace_back([this]() { _test<SIZES[4]>(); });
 		if (m_max_size > 5)
-			_test<SIZES[5]>();
+			threads.emplace_back([this]() { _test<SIZES[5]>(); });
 		if (m_max_size > 6)
-			_test<SIZES[6]>();
+			threads.emplace_back([this]() { _test<SIZES[6]>(); });
 		if (m_max_size > 7)
-			_test<SIZES[7]>();
+			threads.emplace_back([this]() { _test<SIZES[7]>(); });
 		if (m_max_size > 8)
-			_test<SIZES[8]>();
+			threads.emplace_back([this]() { _test<SIZES[8]>(); });
 		if (m_max_size > 9)
-			_test<SIZES[9]>();
+			threads.emplace_back([this]() { _test<SIZES[9]>(); });
+
+		for (auto& thread : threads) {
+			if (thread.joinable()) {
+				thread.join();
+			}
+		}
 	};
 
 private: 
